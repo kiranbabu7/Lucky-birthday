6ed7264058b6c85b9507d5f8f053d152e8ddc744
Kiran Babu <kiranbabu7675@gmail.com>
2021-12-04 03:38:55 +0530

    Feat: App functionality is working

---
+++ app.js
@@ -0,0 +1,39 @@
+const checkBtn = document.querySelector("#check-btn");
+const DobInput = document.querySelector("#dateofbirth");
+const luckyNumber = document.querySelector("#lucky-num");
+const output = document.querySelector(".output");
+const Birthday = DobInput.value;
+let splitDate = [];
+
+checkBtn.addEventListener("click", function () {
+  let sumOfDate = 0;
+  if (luckyNumber.value) {
+    splitDate = DobInput.value.split("");
+    NumDate(splitDate);
+    for (let i = 0; i < splitDate.length; i++) {
+      sumOfDate += Number(splitDate[i]);
+    }
+    let lucky = sumOfDate % luckyNumber.value === 0;
+    updateMsg(lucky);
+  } else {
+    showerror("Enter your Lucky Number");
+  }
+});
+
+function NumDate(date) {
+  while (date.includes("-")) {
+    date.splice(date.indexOf("-"), 1);
+  }
+}
+
+function updateMsg(lucky) {
+  if (lucky) {
+    output.innerText = `${luckyNumber.value} is a Lucky Number 🥳`;
+  } else {
+    output.innerText = `${luckyNumber.value} is Not Lucky Number 😪`;
+  }
+}
+
+function showerror(msg) {
+  output.innerText = msg;
+}
